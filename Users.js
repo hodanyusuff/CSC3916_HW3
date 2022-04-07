6effd7608deb8f50b21f6ab3186d978736ed9551
hodanyusuff <98726595+hodanyusuff@users.noreply.github.com>
2022-04-06 18:29:54 -0600

    Add files via upload

--- Users.js
+++ Users.js
@@ -4,23 +4,17 @@ var bcrypt = require('bcrypt-nodejs');
 
 mongoose.Promise = global.Promise;
 
-//mongoose.connect(process.env.DB, { useNewUrlParser: true });
-try {
-    mongoose.connect( process.env.DB, {useNewUrlParser: true, useUnifiedTopology: true}, () =>
-        console.log("connected"));
-}catch (error) {
-    console.log("could not connect");
-}
-mongoose.set('useCreateIndex', true);
+mongoose.connect(process.env.DB, { useNewUrlParser: true });
+//mongoose.set('useCreateIndex', true);
 
 //user schema
-var UserSchema = new Schema({
+var UserSchema = new Schema( {
     name: String,
-    username: { type: String, required: true, index: { unique: true }},
+    username: {type: String, required: true, index: { unique: true}},
     password: { type: String, required: true, select: false }
 });
 
-UserSchema.pre('save', function(next) {
+UserSchema.pre('save', function(next){
     var user = this;
 
     //hash the password
@@ -37,7 +31,6 @@ UserSchema.pre('save', function(next) {
 
 UserSchema.methods.comparePassword = function (password, callback) {
     var user = this;
-
     bcrypt.compare(password, user.password, function(err, isMatch) {
         callback(isMatch);
     })
